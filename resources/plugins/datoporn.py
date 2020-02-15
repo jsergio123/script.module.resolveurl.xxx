85360c3f137d4244980030cc340c79fa1d9fe334
12asdfg12 <48175015+12asdfg12@users.noreply.github.com>
2020-02-15 16:39:53 +0200

    Update datoporn.py

--- resources/plugins/datoporn.py
+++ resources/plugins/datoporn.py
@@ -18,13 +18,42 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import re
 from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
+from resolveurl.plugins.lib import jsunpack
 
 class DatoPornResolver(ResolveGeneric):
     name = "datoporn"
     domains = ['datoporn.com', 'dato.porn', 'datoporn.co']
     pattern = '(?://|\.)(datoporn\.com|dato\.porn|datoporn\.co)/(?:embed[/-])?([0-9a-zA-Z]+)'
-    
+
+    def __init__(self):
+        self.net = common.Net()
+
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        try:
+            packed = re.search('>(eval\(function\(p,a,c,k,e,d\).+)\s+', html)
+            unpacked = jsunpack.unpack(packed.group(1))
+            r = re.search('src:"([^"]+)"', unpacked)
+            return r.group(1)
+        except Exception as e:
+            common.logger.log_debug(e)
+            raise ResolverError("Video not found")
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
+
     @classmethod
     def _is_enabled(cls):
         return True
+
+
+
+
+
+
